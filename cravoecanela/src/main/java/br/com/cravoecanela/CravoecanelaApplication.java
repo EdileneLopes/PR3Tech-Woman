d67d3bd68567a7b17077ba7731a0fe5e465a32ed
EdileneLopes <60043558+EdileneLopes@users.noreply.github.com>
2022-09-18 14:50:33 -0300

    projeto spring + pacotes
    criado projeto spring + pacotes

---
+++ cravoecanela/src/main/java/br/com/cravoecanela/CravoecanelaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cravoecanela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CravoecanelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CravoecanelaApplication.class, args);
+	}
+
+}
