acef693360e8f246b806a0e6e3d7d79a84817496
EdileneLopes <60043558+EdileneLopes@users.noreply.github.com>
2022-09-24 10:27:03 -0300

    alteração de versão do java, e swagger
    Java na versão 17
    Detalhes no swagger(documentação)

--- cravoecanela/src/main/java/br/com/cravoecanela/swagger/SwaggerConfig.java
+++ cravoecanela/src/main/java/br/com/cravoecanela/swagger/SwaggerConfig.java
@@ -6,15 +6,38 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+
+@EnableSwagger2
 @Configuration
+
 public class SwaggerConfig {
-    @Bean
-    public Docket docket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.cravoecanela"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Documentação (CRUD)")
+				.description("Sistema de Venda de pacotes de viagem - Que permite o Cadastro, Inclusão, Consulta e Exclusão de Clientes.")
+				.version("1.0")
+				.contact(contact())
+				.build();
+	}
+	
+	private Contact contact() {
+		return new Contact("PR3Tech_Woman",
+				"https://github.com/EdileneLopes/PR3Tech-Woman",
+				"lopes_edi@yahoo.com.br");
+	}
 }
