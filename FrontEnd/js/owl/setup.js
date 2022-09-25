dd18b9cece437c23834a352aecc64252f6de7af4
TatiCastro <88949545+TatiCastro@users.noreply.github.com>
2022-09-25 12:14:28 -0700

    Add files via upload
    Reestruturação de pastas e criação login.htm .

---
+++ FrontEnd/js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    
+    margin:40,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
