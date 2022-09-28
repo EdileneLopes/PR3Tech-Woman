94854046689f548fd71caebcbb5173c7532b50da
RaphaMayumi <raphaelamtk@gmail.com>
2022-09-28 00:46:00 -0300

    alteracao index, login e + pg escolha

--- FrontEnd/js/owl/setup.js
+++ FrontEnd/js/owl/setup.js
@@ -1,16 +1,16 @@
 $('.owl-carousel').owlCarousel({
     
-    margin:30,
+    margin:70,
     nav:false,
     responsive:{
         300:{
             items:2
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
-            items:4
+            items:3
         }
     }
 })
